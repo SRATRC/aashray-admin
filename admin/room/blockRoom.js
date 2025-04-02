0a4b48d24c1867cec243cb960d8eb096c22edf0b
Abhishek Jain <abhishek@abhisheks-mbp.lan>
2025-04-02 14:28:34 -0700

    block/unblock room

--- admin/room/blockRoom.js
+++ admin/room/blockRoom.js
@@ -1,33 +1,111 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const blockRoomForm = document.getElementById('blockRoomForm');
+function getAction(room) {
+  switch (room.roomstatus) {
+    case 'available':
+      return `<a href='#' onclick="return block('${room.roomno}')">Block</a>`;
 
-  blockRoomForm.addEventListener('submit', async function (event) {
-    event.preventDefault();
+    case 'blocked':
+      return `<a href='#' onclick="return unblock('${room.roomno}')">Unblock</a>`;
 
-    const roomNo = document.getElementById('roomNo').value.trim();
-    resetAlert();
-    try {
-      const response = await fetch(
-        `${CONFIG.basePath}/stay/block_room/${roomNo}`,
-        {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${sessionStorage.getItem('token')}`
-          }
-          // No body needed for blocking a room
+    default:
+      return '';
+  }
+}
+
+async function block(roomno) {
+  resetAlert();
+  try {
+    const response = await fetch(
+      `${CONFIG.basePath}/stay/block_room/${roomno}`,
+      {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${sessionStorage.getItem('token')}`
         }
-      );
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
       }
+    );
 
-      const data = await response.json();
+    const data = await response.json();
+
+    if (response.ok) {
       showSuccessMessage(data.message);
-    } catch (error) {
-      console.error('Error blocking room:', error);
-      showErrorMessage(error);
+    } else {
+      showErrorMessage(data.message);
     }
-  });
+  } catch (error) {
+    console.error('Error:', error);
+    showErrorMessage(error);
+  }
+}
+
+async function unblock(roomno) {
+  resetAlert();
+  try {
+    const response = await fetch(
+      `${CONFIG.basePath}/stay/unblock_room/${roomno}`,
+      {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${sessionStorage.getItem('token')}`
+        }
+      }
+    );
+
+    const data = await response.json();
+
+    if (response.ok) {
+      showSuccessMessage(data.message);
+    } else {
+      showErrorMessage(data.message);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    showErrorMessage(error);
+  }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', async function () {
+  const tableBody = document.querySelector('#reportTableBody');
+
+  resetAlert();
+
+  try {
+    const response = await fetch(
+      `${CONFIG.basePath}/stay/room_list`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${sessionStorage.getItem('token')}`
+        },
+        body: JSON.stringify() // Default page and page_size
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const rooms = data.data;
+
+    rooms.forEach((room) => {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${room.roomno}</td>
+        <td>${room.roomtype}</td>
+        <td>${room.gender}</td>
+        <td>${room.roomstatus}</td>
+        <td>${getAction(room)}</td>
+      `;
+
+      tableBody.appendChild(row);
+    });
+  } catch (error) {
+    console.error('Error fetching occupancy report:', error);
+    showErrorMessage(error);
+  }
 });
