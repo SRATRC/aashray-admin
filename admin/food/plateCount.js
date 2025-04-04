b67e4d3fc8b5ddc9e00fdebb2acef60ae5ea6d99
Abhishek Jain <abhishek@abhisheks-mbp.lan>
2025-04-04 00:41:57 -0700

    phyical plate count

---
+++ admin/food/plateCount.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', async function () {
+  const plateCountForm = document.getElementById('plateCountForm');
+  
+  resetAlert();
+
+  plateCountForm.addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const date = document.getElementById('date').value;
+    const type = document.getElementById('type').value;
+    const count = document.getElementById('count').value.trim();
+    
+    resetAlert();
+    try {
+      const response = await fetch(
+        `${CONFIG.basePath}/food/physicalPlates`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${sessionStorage.getItem('token')}`
+          },
+          body: JSON.stringify({
+            date,
+            type,
+            count
+          })
+        }
+      );
+
+      const data = await response.json();
+
+      if (response.ok) {
+        showSuccessMessage(data.message);
+      } else {
+        showErrorMessage(data.message);  
+      }
+
+    } catch (error) {
+      console.error('Error issuing physical plate count:', error);
+      showErrorMessage(error);
+    }
+  });
+});
