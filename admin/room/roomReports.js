4921f64e99e74ee69fd26cac3c1ee436a5596cfa
Abhishek Jain <abhishek@abhisheks-mbp.lan>
2025-04-05 18:43:52 -0700

    flat reservation report

--- admin/room/roomReports.js
+++ admin/room/roomReports.js
@@ -26,24 +26,55 @@ function getCancelAction(booking) {
 
 function getEditAction(booking) {
   let editUrl = "";
-  if (booking.status != "cancelled" 
-    && booking.status != "admin cancelled") {
 
-    editUrl = `
-      <a href='updateRoomBooking.html?bookingid=${booking.bookingid}'>
-        <span>&#x270E;</span>
-      </a>`;
+  switch (booking.status) {
+    case "checkedout":
+    case "cancelled":
+    case "admin cancelled":
+      break;
+
+    default:
+      editUrl = `
+        <a href='updateRoomBooking.html?bookingid=${booking.bookingid}'>
+          <span>&#x270E;</span>
+        </a>`;
   }
-  
+
   editUrl += (booking.roomno || "Not Assigned");
   return editUrl;
 }
 
-async function cancel(bookingid) {
+function getFlatAction(booking) {
+  switch (booking.status) {
+    case "pending checkin":
+      return `<a href='#' onclick="return flat_checkin('${booking.CardDb.cardno}')">Check-in</a>`;
+
+    case "checkedin":
+      return `<a href='#' onclick="return flat_checkout('${booking.CardDb.cardno}')">Check-out</a>`;
+
+    default:
+      return "";
+  }
+}
+
+function getFlatCancelAction(booking) {
+  switch (booking.status) {
+    case "checkedin":
+    case "checkedout":
+    case "cancelled":
+    case "admin cancelled":
+      return "";
+
+    default:
+      return `<a href='#' onclick="return flat_cancel('${booking.bookingid}')">Cancel</a>`;
+  }
+}
+
+async function fetchUrl(url) {
   resetAlert();
   try {
     const response = await fetch(
-      `${CONFIG.basePath}/stay/cancel/${bookingid}`,
+      url,
       {
         method: 'PUT',
         headers: {
@@ -56,6 +87,7 @@ async function cancel(bookingid) {
     const data = await response.json();
 
     if (response.ok) {
+      await fetchReport(); 
       showSuccessMessage(data.message);
     } else {
       showErrorMessage(data.message);
@@ -64,66 +96,79 @@ async function cancel(bookingid) {
     console.error('Error:', error);
     showErrorMessage(error);
   }
+}
+
+async function cancel(bookingid) {
+  await fetchUrl(`${CONFIG.basePath}/stay/cancel/${bookingid}`);
 }
 
 async function checkin(cardno) {
-  resetAlert();
-  try {
-    const response = await fetch(
-      `${CONFIG.basePath}/stay/checkin/${cardno}`,
-      {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${sessionStorage.getItem('token')}`
-        },
-        body: JSON.stringify({ cardno })
-      }
-    );
-
-    const data = await response.json();
-
-    if (response.ok) {
-      showSuccessMessage(data.message);
-    } else {
-      showErrorMessage(data.message);
-    }
-  } catch (error) {
-    console.error('Error:', error);
-    showErrorMessage(error);
-  }
+  await fetchUrl(`${CONFIG.basePath}/stay/checkin/${cardno}`);
 }
 
 async function checkout(cardno) {
-  resetAlert();
-  try {
-    const response = await fetch(
-      `${CONFIG.basePath}/stay/checkout/${cardno}`,
-      {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${sessionStorage.getItem('token')}`
-        },
-        body: JSON.stringify({ cardno })
-      }
-    );
-
-    const data = await response.json();
-
-    if (response.ok) {
-      showSuccessMessage(data.message);
-    } else {
-      showErrorMessage(data.message);
-    }
-  } catch (error) {
-    console.error('Error:', error);
-    showErrorMessage(error);
-  }
+  await fetchUrl(`${CONFIG.basePath}/stay/checkout/${cardno}`);
 }
 
-async function fetchReport(reportType, startDate, endDate) {
-  resetAlert();
+async function flat_cancel(bookingid) {
+  await fetchUrl(`${CONFIG.basePath}/stay/flat_cancel/${bookingid}`);
+}
+
+async function flat_checkin(cardno) {
+  await fetchUrl(`${CONFIG.basePath}/stay/flat_checkin/${cardno}`);
+}
+
+async function flat_checkout(cardno) {
+  await fetchUrl(`${CONFIG.basePath}/stay/flat_checkout/${cardno}`);
+}
+
+function createRoomBookingRow(booking, index) {
+  const row = document.createElement('tr');
+  row.innerHTML = `
+    <td>${index + 1}</td>
+    <td>${booking.CardDb.issuedto}</td>
+    <td>${booking.CardDb.mobno}</td>
+    <td>${booking.CardDb.center}</td>
+    <td>${getEditAction(booking)}</td>
+    <td>${booking.roomtype}</td>
+    <td>${booking.checkin}</td>
+    <td>${booking.checkout}</td>
+    <td>${booking.nights}</td>
+    <td>${booking.status}</td>
+    <td>${booking.bookedBy || "Self"}</td>
+    <td>${getAction(booking)}</td>
+    <td>${getCancelAction(booking)}</td>
+  `;
+  return row;
+}
+
+function createFlatBookingRow(booking, index) {
+  const row = document.createElement('tr');
+  row.innerHTML = `
+    <td>${index + 1}</td>
+    <td>${booking.CardDb.issuedto}</td>
+    <td>${booking.CardDb.mobno}</td>
+    <td>${booking.CardDb.center}</td>
+    <td>${booking.flatno}</td>
+    <td>Flat</td>
+    <td>${booking.checkin}</td>
+    <td>${booking.checkout}</td>
+    <td>${booking.nights}</td>
+    <td>${booking.status}</td>
+    <td>${booking.bookedBy || "Self"}</td>
+    <td>${getFlatAction(booking)}</td>
+    <td>${getFlatCancelAction(booking)}</td>
+  `;
+  return row;
+}
+
+
+async function fetchReport() {
+  const reportSelect = document.getElementById('report_type');
+  const reportType = reportSelect.value;
+  
+  const startDate = document.getElementById('start_date').value;
+  const endDate = document.getElementById('end_date').value;
 
   const reportUrl = `${CONFIG.basePath}/stay/${reportType}`;
   try {
@@ -138,11 +183,10 @@ async function fetchReport(reportType, startDate, endDate) {
       }
     );
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
     const data = await response.json();
+    if (!response.ok) {
+      showErrorMessage(data.message);
+    }
 
     const reportsTableBody = document.getElementById('reportTableBody');
     reportsTableBody.innerHTML = '';
@@ -152,30 +196,19 @@ async function fetchReport(reportType, startDate, endDate) {
       return;
     }
 
+    const selectedReport = reportSelect.options[reportSelect.selectedIndex];
+    const roomType = selectedReport.getAttribute('data-type');
+
     data.data.forEach((booking, index) => {
-      const row = document.createElement('tr');
-      row.innerHTML = `
-        <td>${index + 1}</td>
-        <td>${booking.CardDb.issuedto}</td>
-        <td>${booking.CardDb.mobno}</td>
-        <td>${booking.CardDb.center}</td>
-        <td>${getEditAction(booking)}</td>
-        <td>${booking.roomtype}</td>
-        <td>${booking.checkin}</td>
-        <td>${booking.checkout}</td>
-        <td>${booking.nights}</td>
-        <td>${booking.status}</td>
-        <td>${booking.bookedBy || "Self"}</td>
-        <td>${getAction(booking)}</td>
-        <td>${getCancelAction(booking)}</td>
-      `;
+      const row = roomType == 'room' 
+        ? createRoomBookingRow(booking, index)
+        : createFlatBookingRow(booking, index);
       reportsTableBody.appendChild(row);
     });
   } catch (error) {
     console.error('Error fetching report:', error);
     showErrorMessage(error);
   }
-
 }
 
 document.addEventListener('DOMContentLoaded', async function () {
@@ -192,25 +225,13 @@ document.addEventListener('DOMContentLoaded', async function () {
     'reportForm'
   );
 
-  const reportType = document.getElementById('report_type').value;
-  await fetchReport(
-    reportType, 
-    startDate,
-    endDate
-  );
+  resetAlert();
+  await fetchReport();
 
   reportForm.addEventListener('submit', async function (event) {
-    event.preventDefault();
-
-    const reportType = document.getElementById('report_type').value;
-    const startDate = document.getElementById('start_date').value;
-    const endDate = document.getElementById('end_date').value;
-    
-    await fetchReport(
-      reportType, 
-      startDate,
-      endDate
-    );
+    event.preventDefault();   
+    resetAlert(); 
+    await fetchReport();
   });
 });
 
